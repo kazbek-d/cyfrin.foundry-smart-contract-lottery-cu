07648e4ae36678fd57995384e7be2d7d06033004
Anzhela DZARASOVA <angelas82@list.ru>
2026-01-25 09:43:22 +0100

    Initial commit

---
+++ src/Counter.sol
@@ -0,0 +1,47 @@
+// https://docs.soliditylang.org/en/latest/style-guide.html
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title A sample Raffle contract
+ * @author Kazbek DZARASOV
+ * @notice This contract is a creating a simple raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    /** Getter Functions */
+    function get_entranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
